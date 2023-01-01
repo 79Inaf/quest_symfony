7c1b9cc3439de6c5ee2d769fef901a621590226c
79Inaf <fanny.sauvageot@gmail.com>
2023-01-01 13:52:55 +0100

    challenge quest 16 finished - control and validation for the forms new Category and new Program

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,8 +6,10 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[Assert\EnableAutoMapping]
 class Category
 {
     #[ORM\Id]
@@ -16,6 +18,9 @@ class Category
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        max: 255
+    )]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Program::class)]
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -13,7 +13,9 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class);
+            ->add('name', TextType::class, [
+                'attr' => ['maxlenght' => 255]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
